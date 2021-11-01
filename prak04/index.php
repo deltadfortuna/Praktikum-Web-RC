647b0157ffe12c67940577f08b14b1139247942f
deltadfortuna <91016648+deltadfortuna@users.noreply.github.com>
2021-11-01 23:44:35 +0700

    Update index.php

--- prak04/index.php
+++ prak04/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-   <title> 119140184 </title>
+   <title> Prak-mg04 </title>
    <style>
         *{
             font-family: Lucida Bright;
