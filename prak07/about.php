59e7dff6e56939ae309be166da8e40eca2cfcaf1
deltadfortuna <91016648+deltadfortuna@users.noreply.github.com>
2021-11-23 23:40:32 +0700

    Add files via upload

---
+++ prak07/about.php
@@ -0,0 +1,16 @@
+<?php
+    include "connection.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About General Studium</title>
+</head>
+<body>
+    <h2> General Studium </h2>
+
+
+</body>
+</html>
